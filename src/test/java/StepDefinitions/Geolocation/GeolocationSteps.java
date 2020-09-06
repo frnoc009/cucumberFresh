de264aa15db7f75d288c1882d9217f1ae7c986a1
Fernando Castillo <frno009@gmail.com>
2020-09-05 20:16:14 -0700

    maybe final commit

--- src/test/java/StepDefinitions/Geolocation/GeolocationSteps.java
+++ src/test/java/StepDefinitions/Geolocation/GeolocationSteps.java
@@ -1,4 +1,5 @@
 package StepDefinitions.Geolocation;
+
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,12 +9,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class GeolocationSteps {
 
     WebDriver driver = null;
-    public void browser() throws Throwable {
+    WebElement driverElement = null;
+    WebDriverWait driverWait = null;
+
+    public void disableNotificationBrowser() throws Throwable {
         String projectPath=System.getProperty("user.dir");
         System.setProperty("webdriver.chrome.driver",
                 projectPath+"/src/test/resources/driver/chromedriver");
@@ -29,18 +34,24 @@ public class GeolocationSteps {
 
     @Given("User is on the geolocation page")
     public void user_is_on_the_geolocation_page() throws Throwable {
-        browser();
+        disableNotificationBrowser();
         driver.navigate().to("https://the-internet.herokuapp.com/geolocation");
     }
 
     @When("Where am I button is clicked")
-    public void where_am_i_button_is_clicked() {
+    public void where_am_i_button_is_clicked() throws InterruptedException {
+        //driverWait.wait(3);
         driver.findElement(By.xpath("//button[contains(text(),'Where am I?')]")).click();
     }
 
     @Then("the Longitude and latitude are presented")
-    public void the_longitude_and_latitude_are_presented() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_longitude_and_latitude_are_presented() throws InterruptedException {
+        //driverWait.wait(3);
+        driver.findElement(By.xpath("//div[@id='lat-value']")).isDisplayed();
+        String text1 = driver.findElement(By.xpath("//div[@id='lat-value']")).getText();
+        System.out.println(text1);
+        driver.findElement(By.xpath("//div[@id='long-value']")).isDisplayed();
+        String text2 = driver.findElement(By.xpath("//div[@id='long-value']")).getText();
+        System.out.println(text2);
     }
 }
